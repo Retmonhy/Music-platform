5e79b97a6fb7e429ba6cfee4484fdbdfde7e11a8
Dmitry Kuleshov <retmonhy@gmail.com>
2023-05-20 20:44:44 +0700

    Перенесена еще часть на FSD. Track и Playlist и Player в Entity

---
+++ client/src/pages/_app.page.tsx
@@ -0,0 +1,40 @@
+import React, { createContext, useEffect } from 'react';
+import { Provider } from 'react-redux';
+import { AppProps } from 'next/app';
+import { StorageKeys } from '@shared/const';
+import store, { wrapper } from '@shared/store';
+import { useAppDispatch, useAction } from '@shared/hooks';
+import { MainLayout } from '../widgets';
+import { debouncedFetchPl } from './account/playlists.page';
+import '@shared/styles/global.css';
+import '@shared/styles/Global.scss';
+
+export const IsSsrMobileContext = createContext(false);
+const WrappedApp: React.FC<AppProps> = ({ Component, ...pageProps }) => {
+	// const { store, props } = wrapper.useWrappedStore(pageProps); //это строчка создавала дополнительный инстанс стора
+	//так как WrappedApp вызывается при рендере кадой страницы, то наверное будет вызыватьсяэтот юзЭффект всегда
+	const dispatch = useAppDispatch();
+	const { _account } = useAction();
+	useEffect(() => {
+		if (localStorage && localStorage.getItem(StorageKeys.accessToken)) {
+			dispatch(_account.checkAuth())
+				.then(() => {
+					debouncedFetchPl();
+				})
+				.catch(() => {
+					localStorage.removeItem(StorageKeys.accessToken);
+				});
+		}
+	}, []);
+	return (
+		//ts-ignore
+		<IsSsrMobileContext.Provider value={pageProps.pageProps.isSsrMobile}>
+			<Provider store={store}>
+				<MainLayout>
+					<Component {...pageProps} />
+				</MainLayout>
+			</Provider>
+		</IsSsrMobileContext.Provider>
+	);
+};
+export default wrapper.withRedux(WrappedApp);
